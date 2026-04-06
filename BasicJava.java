e3105d6f83f7da13293bffff606081b62e477cab
Yuvraj Singh <ys792608@gmail.com>
2026-04-06 09:33:35 +0530

    Add BasicJava program to calculate average of three numbers

---
+++ BasicJava.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class BasicJava {
+    public static void main(String[] args)
+     {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("enter value of a");
+        int a = sc.nextInt();
+         System.out.println("enter value of b");
+        int b = sc.nextInt();
+        System.out.println("enter value of c");
+        int c = sc.nextInt();
+        int avg = (a+b+c+)/3;
+        System.out.println("Average of three number: "+avg);
+        sc.close();
+
+    }
+}
